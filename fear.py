bc107ad1c0437969bee6738aa6d5447ba2212b0b
sudo-phantom <sudophantom@gmail.com>
2022-01-18 16:51:09 -0700

    create fear class

---
+++ fear.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+class Fear_Greed:
+    '''class to Read from bitcoin fear and greed index'''
+    def __init__(self, name, time, value, classification):
+        
+        
+        self.name = name
+        self.time = time
+        self.value = value
+        self.classification = classification
+        
+    def print_info():
+        '''Method to print the data from  Fear and greed index for bitcoin'''
+        uri = 'https://api.alternative.me/fng/?limit=2'
+
+        r = requests.get(uri) 
+        data = r.json()
+        #def find_actors(self, movies):
+        with open('output.json', 'w') as file:
+            file.write(f"{data}")
+        name = data['name']
+        time = data['data'][0]['timestamp']
+        value = data['data'][0]['value']
+        classification = data['data'][0]['value_classification']
+        endline = "\n-----------------------------\n"
+        print(f"{name}\n\n\tFear level: {value}\n\tRating: {classification}\n\tTimestamp: {time}\n{endline}")
+print(Fear_Greed.print_info())
